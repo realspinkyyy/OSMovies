ef490d13ef34120591fbad416a139d4df65b23ec
realspinkyyy <spinkoliver01@gmail.com>
2025-04-08 21:41:20 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,20 @@
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open("spinktweet-cache").then((cache) => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/styles.css",
-        "/app.js",
-      ]);
-    })
-  );
+  console.log("Service Worker installed");
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      return cachedResponse || fetch(event.request);
-    })
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker activated");
+});
+
+self.addEventListener("push", (event) => {
+  const data = event.data.json();
+  const title = data.title || "Urgent Message";
+  const options = {
+    body: data.message,
+    icon: "icons/icon-192x192.png",
+    badge: "icons/icon-192x192.png",
+  };
+  event.waitUntil(
+    self.registration.showNotification(title, options)
   );
 });
